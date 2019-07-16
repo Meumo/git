450fb089a20d7c868d3813c279625756b68aa859
Mamadou DIOUF <mamadou-d@hotmail.com>
2019-07-16 17:39:22 +0000

    17:38

--- src/main/java/sn/objis/git/presentation/HelloWorld.java
+++ src/main/java/sn/objis/git/presentation/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Merci beaucoup");
+		System.out.println("Bye bye");
 	}
 
 }
